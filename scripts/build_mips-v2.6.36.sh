17921756e90bdb742ee25c7ba11c8da2ec8b1cac
Ariel Alon <arielalon.12321@gmail.com>
2025-10-18 00:02:25 +0300

    Changing configuration in malta build

--- scripts/build_mips-v2.6.36.sh
+++ scripts/build_mips-v2.6.36.sh
@@ -1,8 +1,31 @@
 #!/bin/env bash
 set -e
 
-echo "[*] Configuring kernel for MIPS Malta"
+echo "[*] Generating default config for MIPS Malta"
 make malta_defconfig
+
+echo "[*] Adding pcent32 and e1000 as modules"
+scripts/config --module CONFIG_PCNET32
+scripts/config --module CONFIG_E1000
+
+echo "[*] Using initrd config option"
+scripts/config --enable CONFIG_BLK_DEV_INITRD
+
+echo "[*] Enabling CONFIG_HIGHMEM"
+scripts/config --enable CONFIG_HIGHMEM
+scripts/config --enable CONFIG_HIGHMEM4G
+
+echo "[*] Saving config"
+yes "" | make oldconfig
+
+echo "[*] Building kernel"
+make -j"$(nproc)" vmlinux
+
+echo "[*] Building modules"
+make -j"$(nproc)" modules
+
+echo "[*] Installing modules to /out/modules"
+make modules_install INSTALL_MOD_PATH=/out/modules
 
 echo "[*] Building kernel"
 make -j"$(nproc)"
